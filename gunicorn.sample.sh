c14166e12922a16501684f45d738784e0f441674
Barosl LEE <vcs@barosl.com>
2013-08-08 06:07:17 +0900

    gunicorn.sample.sh: Activate virtual environment before executing

--- gunicorn.sample.sh
+++ gunicorn.sample.sh
@@ -10,4 +10,6 @@ CFG_DIR=~barosl/web/cfgs
 export APP_CFG=$CFG_DIR/$APP.py
 [ -e $APP_CFG ] || { echo 'Configuration file not found.'; exit 1; }
 
+. $VENV/bin/activate || exit 2
+
 cd $DIR && exec $VENV/bin/gunicorn -b 127.0.0.1:$PORT $APP:app
